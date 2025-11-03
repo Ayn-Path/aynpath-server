12d330355183077fbc277712dc880eb494423e77
Mohamad Aiman <104369411+aimanzai@users.noreply.github.com>
2025-11-03 23:56:39 +0800

    Update server.py

--- server.py
+++ server.py
@@ -28,7 +28,7 @@ if not os.path.exists(FEATURES_DIR):
     print("Features extracted successfully!\n")
 
 # --- Config ---
-MAX_DB_DESCRIPTORS = 200000
+MAX_DB_DESCRIPTORS = 50000
 RATIO_THRESH = 0.75
 orb = cv2.ORB_create(nfeatures=2000)
 bf = cv2.BFMatcher(cv2.NORM_HAMMING, crossCheck=False)
