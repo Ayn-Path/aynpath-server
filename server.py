7b72769822bd93e164716c40a8d8507f578b13d6
Mohamad Aiman <104369411+aimanzai@users.noreply.github.com>
2025-11-04 00:14:10 +0800

    Update server.py

--- server.py
+++ server.py
@@ -30,7 +30,7 @@ if not os.path.exists(FEATURES_DIR):
 # --- Config ---
 MAX_DB_DESCRIPTORS = 10000
 RATIO_THRESH = 0.75
-orb = cv2.ORB_create(nfeatures=2000)
+orb = cv2.ORB_create(nfeatures=500)
 bf = cv2.BFMatcher(cv2.NORM_HAMMING, crossCheck=False)
 
 # --- Load database ---
