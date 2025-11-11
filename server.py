d485f7b9e18ef3ac33433222f77fd7a33809f745
Mohamad Aiman <104369411+aimanzai@users.noreply.github.com>
2025-11-12 01:02:41 +0800

    Update server.py

--- server.py
+++ server.py
@@ -28,9 +28,9 @@ if not os.path.exists(FEATURES_DIR):
     print("Features extracted successfully!\n")
 
 # --- Config ---
-MAX_DB_DESCRIPTORS = 15000
+MAX_DB_DESCRIPTORS = 5000
 RATIO_THRESH = 0.75
-orb = cv2.ORB_create(nfeatures=2000)
+orb = cv2.ORB_create(nfeatures=1000)
 bf = cv2.BFMatcher(cv2.NORM_HAMMING, crossCheck=False)
 
 # --- Lazy-load descriptors ---
